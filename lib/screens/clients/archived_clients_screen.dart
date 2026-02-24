f03dc481d6fec52d2fcb1b594ee2fb99ebe1ec00
Tommus129 <tommasoprandini2001@gmail.com>
2026-02-24 23:29:19 +0100

    fix: report cliente build + archivio clienti navigation

--- lib/screens/clients/archived_clients_screen.dart
+++ lib/screens/clients/archived_clients_screen.dart
@@ -284,3 +284,4 @@ class _ArchivedClientsScreenState extends State<ArchivedClientsScreen> {
   }
 }
 
+
--- lib/screens/reports/client_report_screen.dart
+++ lib/screens/reports/client_report_screen.dart
@@ -514,3 +514,4 @@ class _ClientReportScreenState extends State<ClientReportScreen> {
 }
 
 
+
